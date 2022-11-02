d7565469a97e538b9ac5c044cdc1ec87af8b9a3a
Astrid Pulse <astridcpulse@gmail.com>
2022-11-02 16:08:30 -0500

    save some stuff for db setup via aws

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,7 +1,21 @@
 const pg = require('pg');
 
-let pool = new pg.Pool({
-  database: "cloud-todo"
-});
+
+/*
+database url
+
+postgresql://postgres:
+*/
+let pool;
+if (process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL
+  })
+}
+else {
+  pool = new pg.Pool({
+    database: "asty-cloud-todo",      // localhost
+  });
+}
 
 module.exports = pool;
